f263094d0c799c2de3443ab9751ea9d995d37aa1
eric-benny <59672346+eric-benny@users.noreply.github.com>
2020-12-13 20:16:13 -0500

    add url constant

--- src/resources/constants.js
+++ src/resources/constants.js
@@ -1,4 +1,4 @@
 const DEV_URL_BASE = "http://localhost:3000";
-const REMOTE_URL_BASE = "https://wbdv-fa20-ebenny-project-node.herokuapp.com/";
+const REMOTE_URL_BASE = "https://wbdv-fa20-ebenny-project-node.herokuapp.com";
 
 export const URL_BASE = REMOTE_URL_BASE;
